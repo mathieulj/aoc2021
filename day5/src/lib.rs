91ce955de3db3c40b95f0d9e18874323fc367928
Mathieu Letendre-Jauniaux <mathieu.letendre-jauniaux@agilebits.com>
2021-12-05 11:18:26 -0500

    DRY

--- day5/src/lib.rs
+++ day5/src/lib.rs
@@ -28,7 +28,7 @@ fn range(a: i64, b: i64) -> impl Iterator<Item = i64> {
     }
 }
 
-pub fn challenge1(input: &str) -> anyhow::Result<usize> {
+fn challenge(input: &str, diagonals: bool) -> anyhow::Result<usize> {
     let segments = common::parse(input, separated_list1(newline, segment()))?;
     let hot_zones = segments.into_iter().fold(
         HashMap::<(i64, i64), usize>::new(),
@@ -41,31 +41,7 @@ pub fn challenge1(input: &str) -> anyhow::Result<usize> {
                 for x in range(x1, x2) {
                     *acc.entry((x, y1)).or_default() += 1;
                 }
-            }
-            acc
-        },
-    );
-
-    Ok(hot_zones
-        .into_iter()
-        .filter(|(_, count)| *count > 1)
-        .count())
-}
-
-pub fn challenge2(input: &str) -> anyhow::Result<usize> {
-    let segments = common::parse(input, separated_list1(newline, segment()))?;
-    let hot_zones = segments.into_iter().fold(
-        HashMap::<(i64, i64), usize>::new(),
-        |mut acc, ((x1, y1), (x2, y2))| {
-            if x1 == x2 {
-                for y in range(y1, y2) {
-                    *acc.entry((x1, y)).or_default() += 1;
-                }
-            } else if y1 == y2 {
-                for x in range(x1, x2) {
-                    *acc.entry((x, y1)).or_default() += 1;
-                }
-            } else {
+            } else if diagonals {
                 for (x, y) in range(x1, x2).zip(range(y1, y2)) {
                     *acc.entry((x, y)).or_default() += 1;
                 }
@@ -79,6 +55,14 @@ pub fn challenge2(input: &str) -> anyhow::Result<usize> {
         .into_iter()
         .filter(|(_, count)| *count > 1)
         .count())
+}
+
+pub fn challenge1(input: &str) -> anyhow::Result<usize> {
+    challenge(input, false)
+}
+
+pub fn challenge2(input: &str) -> anyhow::Result<usize> {
+    challenge(input, true)
 }
 
 #[cfg(test)]
